3d0141d2815a82a43bd178c27aa847838b9c6949
Hassan Murtaza <hassan.murtaza@smartfunstudios.com>
2024-07-23 16:29:02 +0500

    Indicate mistake is startup structure

--- DB/db.js
+++ DB/db.js
@@ -2,13 +2,13 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     const connectionInstance = await mongoose.connect(
-      "mongodb://localhost:27017"
+      "mongodb://localhost:27017" // Here You not add database name like correct URL will be mongodb://localhost:27017/UserManagementSystem
     );
     console.log(
       `DB Connected ||  DB host is  ${connectionInstance.connection.host}`
     );
   } catch (error) {
-    console.log("An error occured while DB is connected");
+    console.log("An error occured while DB is connected"); // correct your message => "An error occured while connecting with db"
   }
 };
 module.exports = { connectDB };
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# Node_User_Management_System
-Complete use management system
+# User Management System
+
+- Make folders name small letters like controllers, models, routes
+
+- convert DB folder into config folder and file in it will be db.config.js
+
+- Also change file name in other folders like user.controllers.js, user.routes.js, user.model.js
+
+- Change all fields name PascalCase to camelCase in model file and use camelCase for createing file name if file name have more then one words like js file name example will be emailValidator (this is because in javascript the best case is camelCase so that's why)
